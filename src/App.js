7a4609df8d3dce92ad3b79cf80bbfbda2b387908
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-06-09 15:09:03 -0500

    set up HomePage with state to track lists and function to pull songs from database; SongList accepting props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
+import react from "react";
 import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
+
   return (
     <div className="App">
       <HomePage/>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import SongList from '../components/SongList'
 import FavoriteList from '../components/FavoriteList'
 
 const HomePage = (props)=>{
+      //BACKEND URL 
+        const url = "https://tuner-playlist.herokuapp.com/"
+
+        //empty song
+        const emptySong = {
+            title:"",
+            artist:"",
+            time:"",
+            favorite: false
+        }
+
+        //instances of state to handle song lists
+        
+        const [songs, setSongs] = useState([emptySong ])
+        const [favoriteSongs, setFavoriteSongs] = useState([])
+
+        // getSongs function 
+        const getSongs = () => { 
+            fetch(url + '/songs/')
+            .then((response) => response.json())
+            .then((data) => setSongs(data))
+            }
+
+        //get all songs on screen load
+        useEffect(()=>{
+            getSongs()
+        },[])
+
     return (
         <>
-            <SongList/>
+            <SongList songs={songs}/>
             <FavoriteList/>
         </>
     )
